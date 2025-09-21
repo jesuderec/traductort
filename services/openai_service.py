efa067066b66c8bbe61b87dc73b4921cfc13122d
jesuderec <jesuderec@gmail.com>
2025-09-21 10:57:16 -0600

    Update openai_service.py

--- services/openai_service.py
+++ services/openai_service.py
@@ -1,22 +1,22 @@
-import logging
 from openai import OpenAI
 from config.settings import settings
 from config.prompts import TranslationPrompts
+import logging
 
 logger = logging.getLogger(__name__)
 
 class OpenAIService:
     @classmethod
-    def translate(cls, text, target_language, prompt=None, model=None):
-        logger.info(f"Iniciando llamada a la API de OpenAI para traducir a {target_language}.")
+    def translate(cls, text, source_language, target_language, prompt=None, model=None):
+        logger.info(f"Iniciando llamada a la API de OpenAI para traducir de {source_language} a {target_language}.")
         if not prompt:
-            prompt = TranslationPrompts.OPENAI_PROMPT.format(target_language=target_language)
-        
+            prompt = TranslationPrompts.OPENAI_PROMPT.format(source_language=source_language, target_language=target_language)
+
         if not model:
             model = settings.OPENAI_MODEL
-        
+
         client = OpenAI(api_key=settings.OPENAI_API_KEY, timeout=settings.TIMEOUT)
-        
+
         try:
             response = client.chat.completions.create(
                 model=model,
